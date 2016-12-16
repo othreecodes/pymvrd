f28c8b9506d9a597dddfb9452f64b1a2bf16932b
Obi Uchenna David <daviduchenna@outlook.com>
2016-12-16 14:23:39 +0100

    Initial commit

---
+++ pymvrd/errors/errors.py
@@ -0,0 +1,11 @@
+class Mvrd(Exception):
+    '''
+    MVRD Exception
+    '''
+    pass
+
+class InvalidPlateError(Exception):
+    '''
+    The Plate number Entered was Invalid
+    '''
+
---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'pymvrd',
+  packages = ['pymvrd'],
+  version = '0.1',
+  description = 'Motor Vehicle Registration Information Search Portal Library',
+  author = 'Obi Uchenna David',
+  author_email = 'daviduchenna@outlook.com',
+  url = 'https://github.com/othreecodes/pymvrd', 
+  download_url = 'https://github.com/othreecodes/pymvrd/tarball/0.1', 
+  keywords = ['vehicle','plate number','registration'], 
+  classifiers = [],
+)
