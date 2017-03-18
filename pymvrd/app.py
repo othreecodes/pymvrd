352409d6826806522d563c79da86ddf202d43c9f
Obi Uchenna David <daviduchenna@outlook.com>
2017-03-18 09:17:57 +0100

    added signature

--- pymvrd/app.py
+++ pymvrd/app.py
@@ -4,6 +4,12 @@
 pyMVRD
 Nigerian Motor Vehicle Registration Lookup
 __author__ = 'othreecodes'
+  ___  _   _                                 _           
+ / _ \| |_| |__  _ __ ___  ___  ___ ___   __| | ___  ___ 
+| | | | __| '_ \| '__/ _ \/ _ \/ __/ _ \ / _` |/ _ \/ __|
+| |_| | |_| | | | | |  __/  __/ (_| (_) | (_| |  __/\__ \
+ \___/ \__|_| |_|_|  \___|\___|\___\___/ \__,_|\___||___/
+
 '''
 import requests
 from bs4 import BeautifulSoup
